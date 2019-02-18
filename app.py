c08347bc95587841990ac4f7df3c2045b7b36a78
Danny Ryman <danny.ryman@dootrix.com>
2019-02-18 13:44:02 +0000

    Added database support for user registration

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ from passlib.hash import sha256_crypt
 app = Flask(__name__)
 
 Starters = Starters()
+#config MYSQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'newpassword'
+app.config['MYSQL_DB'] = 'restaurant'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#init MYSQL
+
+mysql= MySQL(app)
 
 @app.route('/')
 def index():
@@ -20,7 +30,7 @@ def starter(id):
         return render_template('starter.html', id = id)
 
 class RegisterForm(Form):
-        name = StringField('Name', [validators.Length(min=1, max = 50)])
+        name = StringField('Name', [validators.Length(min=1)])
         username = StringField('Username', [validators.Length(min=4,max=25)])
         email = StringField('Email', [validators.Length(min =6, max = 50)])
         password = PasswordField('Password', [
@@ -34,9 +44,23 @@ class RegisterForm(Form):
 def register():
         form = RegisterForm(request.form)
         if request.method == 'POST' and form.validate():
-                return render_template('register.html')
+                name = form.name.data
+                email = form.email.data
+                username = form.username.data
+                password = sha256_crypt.encrypt(str(form.password.data))
+
+                cur = mysql.connection.cursor()
+                cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",(name, email, username, password))
+
+                mysql.connection.commit()
+
+                cur.close()
+                
+                flash("You are now registered! You can now log in!", "success")
+                return redirect(url_for('login'))
         return render_template('register.html', form = form)
 
 
 if __name__ == '__main__':
+    app.secret_key ='secret123'
     app.run(debug = True)

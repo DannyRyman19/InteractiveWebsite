d2dafdd29fcf21df298937e80336c7235fc7908e
Danny Ryman <danny.ryman@dootrix.com>
2019-02-18 12:59:27 +0000

    Added a registration page with form validatio (not linked to database just yet). Also added data to the starters page

---
+++ data.py
@@ -0,0 +1,64 @@
+def Starters():
+    starters = [
+        {
+            'id' : 1,
+            'item_name' : 'Garlic Bread',
+            'item_description' : '',
+            'price' : '4.95',
+            'suitable_for_vegetarians' : True 
+
+        },
+
+         {
+            'id' : 2,
+            'item_name' : 'Marinated Olives',
+            'item_description' : '',
+            'price' : '3.95',
+            'suitable_for_vegetarians' : True 
+        },
+
+         {
+            'id' : 3,
+            'item_name' : 'Freshly Baked Breadsticks',
+            'item_description' : 'Served with Pesto Mayonnaise.',
+            'price' : '5.15',
+            'suitable_for_vegetarians' : True 
+        },
+
+         {
+            'id' : 4,
+            'item_name' : 'Mozzarella Garlic Bread',
+            'item_description' : 'With Caramelised onions.',
+            'price' : '5.95',
+            'suitable_for_vegetarians' : False
+        },
+
+         {
+            'id' : 5,
+            'item_name' : 'Baked Brie',
+            'item_description' : 'Honey, hazelnuts, roasted garlic, black pepper, toasted sourdough.',
+            'price' : '6.75',
+            'suitable_for_vegetarians' : False
+        },
+
+           {
+            'id' : 6,
+            'item_name' : 'Calamari',
+            'item_description' : 'Served with cajun mayo dipping sauce.',
+            'price' : '6.95',
+            'suitable_for_vegetarians' : False
+
+        },
+
+         {
+            'id' : 7,
+            'item_name' : 'Tallegio & Wild Mushroom Crostini',
+            'item_description' : 'Chestnut, shiitake & oyster mushroom, melted taleggio cheese, truffle oil, thyme, on toasted sourdough',
+            'price' : '6.85',
+            'suitable_for_vegetarians' : True 
+
+        }
+
+
+    ]
+    return starters

a30cd0373525f43d31e26be5abca7659ba71e96f
Danny Ryman <danny.ryman@dootrix.com>
2019-02-19 09:02:17 +0000

    Added login page, dashboard page. Users can log out and must be logged in to access dashboard page

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from data import Starters
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
+
 app = Flask(__name__)
 
 Starters = Starters()
@@ -21,14 +23,17 @@ mysql= MySQL(app)
 def index():
         return render_template('home.html')
 
+# starters
 @app.route('/starters')
 def starters():
         return render_template('starters.html', starters = Starters)
 
+#individual food pages
 @app.route('/starter/<string:id>/')
 def starter(id):
         return render_template('starter.html', id = id)
 
+#sign up form validation
 class RegisterForm(Form):
         name = StringField('Name', [validators.Length(min=1)])
         username = StringField('Username', [validators.Length(min=4,max=25)])
@@ -40,6 +45,7 @@ class RegisterForm(Form):
         ])
         confirm = PasswordField('Confirm Password')
 
+#Register
 @app.route('/register',methods=['GET','POST'])
 def register():
         form = RegisterForm(request.form)
@@ -47,10 +53,10 @@ def register():
                 name = form.name.data
                 email = form.email.data
                 username = form.username.data
-                password = sha256_crypt.encrypt(str(form.password.data))
+                password = sha256_crypt.encrypt(str(form.password.data)) #encrypts password
 
                 cur = mysql.connection.cursor()
-                cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",(name, email, username, password))
+                cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",(name, email, username, password)) #adds all registration information into database
 
                 mysql.connection.commit()
 
@@ -60,7 +66,59 @@ def register():
                 return redirect(url_for('login'))
         return render_template('register.html', form = form)
 
+#login
+@app.route('/login', methods = ['GET','POST'])
+def login():
+        if request.method == 'POST':
+                username = request.form['username']
+                password_candidate = request.form['password']
+
+                cur = mysql.connection.cursor()
+                result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+                if result > 0: #checks if username exists
+                        data = cur.fetchone()
+                        password = data['password']
+                        if sha256_crypt.verify(password_candidate, password): #checks if passwords match
+                                session['logged_in'] = True
+                                session['username'] = username
+
+                                flash('You are now logged in', 'success')
+                                return redirect(url_for('dashboard')) #logs user in to dashboard
+                        else: #passswords don't match
+                                error = "Login Failed"
+                                return render_template('login.html', error = error)
+                        cur.close() #closes connection to database
+                else: #user doesn't exist
+                        error = "Username not found"
+                        return render_template('login.html', error = error)
+        
+        return render_template('login.html')
+
+#login check - prevents non logged in users from accesing dashboard page
+def is_logged_in(f):
+        @wraps(f)
+        def wrap(*args, **kwargs):
+                if 'logged_in' in session:
+                        return f(*args, **kwargs)
+                else:
+                        flash("You must be logged in to access this page", 'danger')
+                        return redirect(url_for('login'))
+        return wrap
+#Logout
+@app.route('/logout')
+def logout():
+        session.clear() 
+        flash("You have been logged out!", 'success')
+        return redirect(url_for('login'))
+
+#Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+        return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.secret_key ='secret123'
     app.run(debug = True)
+    

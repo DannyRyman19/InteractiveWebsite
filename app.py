a19f87f36d2ddf1d6ace38553fcb1cc2645fddc6
Danny Ryman <danny.ryman@dootrix.com>
2019-03-04 13:48:12 +0000

    Added pages for each category.

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from data import Starters
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, DecimalField, BooleanField, SelectField, IntegerField, ValidationError
 from passlib.hash import sha256_crypt
 from functools import wraps
 
 app = Flask(__name__)
 
-Starters = Starters()
 #config MYSQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -17,54 +15,129 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 #init MYSQL
 
-mysql= MySQL(app)
+#login check - prevents non logged in users from accesing dashboard page
+def is_logged_in(f):
+        @wraps(f)
+        def wrap(*args, **kwargs):
+                if 'logged_in' in session:
+                        return f(*args, **kwargs)
+                else:
+                        flash("You must be logged in to access this page", 'danger')
+                        return redirect(url_for('login'))
+        return wrap
 
+
+     
 @app.route('/')
 def index():
         return render_template('home.html')
 
+#Product creation form
+ 
+
+
+
+#drinks
+@app.route('/drinks')
+def drinks():
+       
+        cur = mysql.connection.cursor()
+
+        cur.execute("SELECT * FROM category, sub_category WHERE category.category_id = sub_category.category_id AND category.category_id = 1")
+        
+       
+        sub_categories = cur.fetchall()
+        cur1 = mysql.connection.cursor()
+        sub=[]
+        for subcat in sub_categories:
+                cur1.execute(("SELECT * FROM product, product_variation WHERE product.subcategory_id = {0} AND product.product_id = product_variation.product_id").format((str((subcat["subcategory_id"])))))
+                sub.append([subcat["subcategory_name"],cur1.fetchall()])
+                #print(subcat["subcategory_id"])
+
+      
+
+        cur.close()
+     
+        
+        return render_template('drinks.html',  sub_categories = sub)
+
+
 # starters
 @app.route('/starters')
 def starters():
-        return render_template('starters.html', starters = Starters)
+        cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * FROM product_variation, product, sub_category, category WHERE product_variation.product_id = product.product_id AND product.subcategory_id = sub_category.subcategory_id AND sub_category.category_id = 2 and category.category_id = 2;")
 
-#individual food pages
-@app.route('/starter/<string:id>/')
-def starter(id):
-        return render_template('starter.html', id = id)
+        starters= cur.fetchall()
+        cur.close()
+       
+        return render_template('starters.html', starters=starters)
+       
+   
+
+
+
+# sides
+@app.route('/sides')
+def sides():
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM product_variation, product, sub_category, category WHERE product_variation.product_id = product.product_id AND product.subcategory_id = sub_category.subcategory_id AND sub_category.category_id = 4 and category.category_id = 4;")
+
+        sides= cur.fetchall()
+        cur.close()
+       
+        return render_template('sides.html', sides=sides)
+
+
+# mains
+@app.route('/mains')
+def mains():
+        cur = mysql.connection.cursor()
+
+        cur.execute("SELECT * FROM category, sub_category WHERE category.category_id = sub_category.category_id AND category.category_id = 3")
+        
+       
+        sub_categories = cur.fetchall()
+        cur1 = mysql.connection.cursor()
+        sub=[]
+        for subcat in sub_categories:
+                cur1.execute(("SELECT * FROM product, product_variation WHERE product.subcategory_id = {0} AND product.product_id = product_variation.product_id").format((str((subcat["subcategory_id"])))))
+                sub.append([subcat["subcategory_name"],cur1.fetchall()])
+                #print(subcat["subcategory_id"])
+
+      
+
+        cur.close()
+     
+        
+        return render_template('mains.html',  sub_categories = sub)
+
+# desserts
+@app.route('/desserts')
+def desserts():
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM product_variation, product, sub_category, category WHERE product_variation.product_id = product.product_id AND product.subcategory_id = sub_category.subcategory_id AND sub_category.category_id = 5 and category.category_id = 5;")
+
+        desserts = cur.fetchall()
+        cur.close()
+       
+        return render_template('desserts.html', desserts=desserts)
+
+
 
 #sign up form validation
 class RegisterForm(Form):
         name = StringField('Name', [validators.Length(min=1)])
         username = StringField('Username', [validators.Length(min=4,max=25)])
         email = StringField('Email', [validators.Length(min =6, max = 50)])
+        authority = SelectField('Authority', choices=[('1','General Manager'),('2','Assistant Manager'),('3','Supervisor'),('4','Waiter')])
         password = PasswordField('Password', [
                 validators.DataRequired(),
                 validators.EqualTo('confirm', message = "Passwords do not match")
 
         ])
         confirm = PasswordField('Confirm Password')
-
-#Register
-@app.route('/register',methods=['GET','POST'])
-def register():
-        form = RegisterForm(request.form)
-        if request.method == 'POST' and form.validate():
-                name = form.name.data
-                email = form.email.data
-                username = form.username.data
-                password = sha256_crypt.encrypt(str(form.password.data)) #encrypts password
-
-                cur = mysql.connection.cursor()
-                cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",(name, email, username, password)) #adds all registration information into database
-
-                mysql.connection.commit()
-
-                cur.close()
-                
-                flash("You are now registered! You can now log in!", "success")
-                return redirect(url_for('login'))
-        return render_template('register.html', form = form)
+      
 
 #login
 @app.route('/login', methods = ['GET','POST'])
@@ -72,17 +145,43 @@ def login():
         if request.method == 'POST':
                 username = request.form['username']
                 password_candidate = request.form['password']
-
+                
                 cur = mysql.connection.cursor()
                 result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
-
+               
                 if result > 0: #checks if username exists
+                       
                         data = cur.fetchone()
+                        cur.execute("SELECT authority FROM users WHERE username = %s", [username]) 
+                        auth = cur.fetchone() 
+                        cur.execute("SELECT id FROM users WHERE username = %s", [username]) 
+                        userID = cur.fetchone() 
+                        cur.execute("SELECT name FROM users WHERE username = %s", [username]) 
+                        name = cur.fetchone() 
+                        name = str(name)
+                        name = name[10:-2]                              
                         password = data['password']
+                        auth = str(auth)
+                        userID = str(userID)
+                        userID = userID[6:-1]
+                        userID = int(userID)
+                        if auth == "{'authority': 1}":
+                                auth = "General Manager"
+                                session['manager'] = True
+                        elif auth == "{'authority': 2}":
+                                auth = "Assistant Manager"
+                                session['manager'] = True
+                        elif auth == "{'authority': 3":
+                                auth = "Supervisor"
+                        else:
+                                auth = "Waiter"
                         if sha256_crypt.verify(password_candidate, password): #checks if passwords match
+                                session['auth'] = auth
                                 session['logged_in'] = True
                                 session['username'] = username
-
+                                session['password'] = password
+                                session['name'] = name
+                                session['id'] = userID
                                 flash('You are now logged in', 'success')
                                 return redirect(url_for('dashboard')) #logs user in to dashboard
                         else: #passswords don't match
@@ -95,30 +194,27 @@ def login():
         
         return render_template('login.html')
 
-#login check - prevents non logged in users from accesing dashboard page
-def is_logged_in(f):
-        @wraps(f)
-        def wrap(*args, **kwargs):
-                if 'logged_in' in session:
-                        return f(*args, **kwargs)
-                else:
-                        flash("You must be logged in to access this page", 'danger')
-                        return redirect(url_for('login'))
-        return wrap
+
+
+
 #Logout
 @app.route('/logout')
+@is_logged_in
 def logout():
         session.clear() 
         flash("You have been logged out!", 'success')
         return redirect(url_for('login'))
 
-#Dashboard
+
+
 @app.route('/dashboard')
 @is_logged_in
+@is_manager
 def dashboard():
         return render_template('dashboard.html')
+
+
 
 if __name__ == '__main__':
     app.secret_key ='secret123'
     app.run(debug = True)
-    
